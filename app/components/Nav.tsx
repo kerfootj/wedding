758720cb4467d1cff4444cc13d9899719a8902c7
kerfootj <jkerfoot@paystone.com>
2023-09-13 23:04:31 -0700

    setup nextjs

---
+++ app/components/Nav.tsx
@@ -0,0 +1,84 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+const links = [
+    {
+        name: 'Home',
+        href: '#',
+    },
+    {
+        name: 'Our Story',
+        href: '#story',
+    },
+    {
+        name: 'Where & When',
+        href: '#where-and-when',
+    },
+    {
+        name: 'Gallery',
+        href: '#gallery',
+    },
+    {
+        name: 'RSVP',
+        href: '#rsvp',
+    },
+];
+
+export function Nav() {
+    const [selected, setSelected] = useState('');
+    const [position, setPosition] = useState(0);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setPosition(window.scrollY);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => window.removeEventListener('scroll', handleScroll);
+    });
+
+    return (
+        <header
+            className={`fixed z-50 flex w-full select-none justify-center border-b px-4 transition-colors duration-500 sm:px-6 lg:px-8 ${
+                position < 32 ? 'bg-transparent' : 'bg-white'
+            }`}
+        >
+            <div
+                className={`flex h-16 w-full max-w-7xl justify-between transition-colors duration-300 ${
+                    position < 32 ? 'text-gray-100' : 'text-neutral-900'
+                }`}
+            >
+                <div className="flex flex-shrink-0 items-center">
+                    <a href="#">
+                        <p className="font-cursive text-4xl font-semibold tracking-wider">
+                            Leia & Joel
+                        </p>
+                    </a>
+                </div>
+                <div className="hidden items-center justify-center gap-8 text-center sm:flex">
+                    {links.map((link, index) => (
+                        <>
+                            {index > 0 && (
+                                <i className="fas fa-heart fa-2xs text-violet-300" />
+                            )}
+                            <a
+                                key={link.name}
+                                href={link.href}
+                                className={`inline-flex items-center text-sm font-semibold transition-colors ${
+                                    selected === link.name
+                                        ? 'text-violet-300'
+                                        : 'hover:text-violet-200'
+                                }`}
+                                onClick={() => setSelected(link.name)}
+                            >
+                                {link.name}
+                            </a>
+                        </>
+                    ))}
+                </div>
+            </div>
+        </header>
+    );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,32 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './components/**/*.{js,ts,jsx,tsx,mdx}',
+        './app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+                'gradient-conic':
+                    'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+            },
+            dropShadow: {
+                dark: [
+                    '0 1px 2px rgb(0 0 0 / 0.40)',
+                    '0 1px 1px rgb(0 0 0 / 0.06)',
+                ],
+            },
+        },
+        fontFamily: {
+            sans: ['var(--font-poppins)'],
+            serif: ['var(--font-alex-brush)'],
+            cursive: ['var(--font-de-haviland)'],
+        },
     },
-  },
-  plugins: [],
-}
-export default config
+    plugins: [],
+};
+
+export default config;
