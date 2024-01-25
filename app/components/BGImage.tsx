844824efc782c7ddcb753f814b655451fcb61e58
kerfootj <joel.k@nicejob.com>
2024-01-24 23:14:09 -0800

    attempted fix for over zoomed images on iOS

--- app/components/BGImage.tsx
+++ app/components/BGImage.tsx
@@ -5,9 +5,16 @@ import { PropsWithChildren } from 'react';
 export function BGImage(props: PropsWithChildren<{ imageUrl: string }>) {
     const { children, imageUrl } = props;
 
+    function iOS() {
+        // iOS detection from: http://stackoverflow.com/a/9039885/177710
+        return /iPad|iPhone|iPod/.test(navigator.userAgent);
+    }
+
     return (
         <div
-            className={`h-screen w-screen bg-cover bg-fixed bg-center`}
+            className={`h-screen w-screen bg-cover bg-center ${
+                iOS() ? 'bg-scroll ' : 'bg-fixed'
+            }`}
             style={{ backgroundImage: `url('${imageUrl}')` }}
         >
             {children}
